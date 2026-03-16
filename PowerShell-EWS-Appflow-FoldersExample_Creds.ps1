826cb0f52128dbe1f3b684e9371a68c139922fa9
Daniel Bagley <dseph@users.noreply.github.com>
2026-03-16 17:08:13 -0400

    Create PowerShell-EWS-Appflow-FoldersExample_Creds.ps1

---
+++ PowerShell-EWS-Appflow-FoldersExample_Creds.ps1
@@ -0,0 +1,11 @@
+# PowerShell-EWS-Appflow-FoldersExample_Creds.ps1
+
+# ---------------------
+# Testing - begin
+
+#$TenantId     = "<TENANT_ID_GUID>"
+#$ClientId     = "<APP_CLIENT_ID_GUID>"
+#$ClientSecret = "<APP_CLIENT_SECRET_VALUE>"
+
+# Testing - end
+# ---------------------
